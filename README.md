985ff5477a1fd897493f7109172dc7889cbde306
Lokesh Murali <61135349+lokemurali@users.noreply.github.com>
2024-02-01 17:37:20 +0000

    second commit

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ docker run -it \
  docker run -it \
     -e PGADMIN_DEFAULT_EMAIL="admin@admin.com" \
     -e PGADMIN_DEFAULT_PASSWORD="root" \
-    -p 8080:80 \
+    -p 8000:80 \
     --network=pg-network \
     --name pgadmin \
     dpage/pgadmin4
--- ingest_data.py
+++ ingest_data.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# coding: utf-8
+
 import os
 import argparse
 import pandas as pd
@@ -5,15 +8,19 @@ from time import time
 from sqlalchemy import create_engine
 import pyarrow.parquet as pq
 import pyarrow as pa
+from dotenv import load_dotenv,dotenv_values
+
 
 def main():
-    user = os.environ['POSTGRES_USER']
-    password = os.environ['POSTGRES_PASSWORD']
-    host = os.environ['HOST']
-    port = os.environ['PORT']
-    db = os.environ['POSTGRES_DB']
-    table_name = os.environ['TABLE_NAME']
-    url = os.environ['URL'] 
+    load_dotenv()
+    print(os.getenv("POSTGRES_USER"))
+    user = os.environ('POSTGRES_USER')
+    password = os.environ('POSTGRES_PASSWORD')
+    host = os.environ('HOST')
+    port = os.environ('PORT')
+    db = os.environ('POSTGRES_DB')
+    table_name = os.environ('TABLE_NAME')
+    url = os.environ('URL')
 
     engine = create_engine(
         f'postgresql://{user}:{password}@{host}:{port}/{db}')
@@ -28,7 +35,7 @@ def main():
     for batch in parquet_file.iter_batches(batch_size=50000):
         t_start = time()
 
-        table = pa.Table.from_batches([batch])
+        table = pa.Table.from_batches((batch))
         df = table.to_pandas(split_blocks=True, self_destruct=True)
 
         if flag == False:
@@ -44,3 +51,7 @@ def main():
         print('inserted another chunk, took %.3f second' % (t_end-t_start))
 
     print("Finished ingesting data into the postgres database")
+
+
+if __name__ == '__main__':
+    main()
